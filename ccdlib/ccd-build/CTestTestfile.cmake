e251c22e71dafd60dbaea3ffc77a49cc24928f06
Myelin Sheath <actinfiber@gmail.com>
2024-04-29 17:42:45 +0500

    ccd import

---
+++ ccdlib/ccd-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: D:/engineslist/px/mujoco/24/04/25/mjmod/Bvs/_deps/ccd-src
+# Build directory: D:/engineslist/px/mujoco/24/04/25/mjmod/Bvs/_deps/ccd-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
---
+++ ccdlib/ccd-build/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/engineslist/px/mujoco/24/04/25/mjmod/Bvs/_deps/ccd-src/src
+# Build directory: D:/engineslist/px/mujoco/24/04/25/mjmod/Bvs/_deps/ccd-build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
